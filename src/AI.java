2b63a209c540763936ec685533980e17012ceb3d
Steve Rice <srice@emich.edu>
2020-11-14 11:37:44 -0500

    only valid moves

--- src/AI.java
+++ src/AI.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class AI {
 
 	public AI() {}
 
 	public int computeMove(GameState state) {
-		System.out.println("AI returning canned move for game state - " + state);
-		return 1;
+		ArrayList<Integer> valid_moves = new ArrayList<>();
+		int[][] board = state.getBoard();
+		for (int i = 0; i < 7; i++) if (board[0][i] == 0) valid_moves.add(i);
+		return valid_moves.get((new Random()).nextInt(valid_moves.size()));
 	}
 }
